54d83461567e4115140066d16fc735dea49b7f9d
cwittwer <wittwer.carson@vt.edu>
2021-04-26 19:37:14 -0700

    Update with new pages
    -added multiple pages for different vehicle searches
    -style editing
    -connection with Datastore

--- main.py
+++ main.py
@@ -1,28 +1,85 @@
 import datetime
 
 from flask import Flask, render_template
+from google.cloud import datastore
+
+datastore_client = datastore.Client()
 
 app = Flask(__name__)
 
+def store_time(dt):
+    entity = datastore.Entity(key=datastore_client.key('visit'))
+    entity.update({
+        'timestamp' : dt
+    })
+
+    datastore_client.put(entity)
+
+def fetch_times(limit):
+    query = datastore_client.query(kind='visit')
+    query.order = ['-timestamp']
+
+    times = query.fetch(limit=limit)
+
+    return times
+
+def fetch_listings(lim, query_name):
+    query = datastore_client.query(kind=query_name)
+    query.order = ['-timestamp']
+
+    listings = query.fetch(limit=lim)
+
+    return listings
 
 @app.route('/')
 def index():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
+    # Store the current access time in Datastore
+    store_time(datetime.datetime.now())
 
-    return render_template('index.html', times=dummy_times)
+    #Fetch the most recent 10 access times from datastore.
+    times = fetch_times(10)
+
+    return render_template('index.html', times=times)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/infinite_scroll')
-def infinite_scroll():
-    return render_template('infinite_scroll.html')
+@app.route('/classic-bimmers')
+def classic_bimmers():
+
+    cars = fetch_listings(20, 'BMW')
+
+    return render_template('classic_bimmers.html', cars=cars)
+
+@app.route('/m-cars')
+def mcars():
+
+    cars = fetch_listings(20, 'mcar')
+
+    return render_template('mcars.html', cars=cars)
+
+@app.route('/miata-hunter')
+def miata_hunter():
+
+    cars = fetch_listings(20, 'miata')
+
+    return render_template('miata_hunter.html', cars=cars)
+
+@app.route('/swedish-steel')
+def swedish_steel():
+
+    cars = fetch_listings(20, 'volvo')
+    cars2 = fetch_listings(20, 'Saab')
+
+    return render_template('swedish_steel.html', cars=cars)
+    
+@app.route('/barely-driven')
+def barely_driven():
+
+    cars = fetch_listings(20, 'barely')
+
+    return render_template('barely_driven.html', cars=cars)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
