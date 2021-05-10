bba55874799e21870455af98ef5abd04e63a65a8
cwittwer <wittwer.carson@vt.edu>
2021-05-09 22:29:30 -0700

    Starting to add pagination
    adding querying updates to main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from google.cloud import datastore
 
 datastore_client = datastore.Client()
 
 app = Flask(__name__)
+LISTINGS_PER_PAGE=20
 
 def store_time(dt):
     entity = datastore.Entity(key=datastore_client.key('visit'))
@@ -23,16 +24,20 @@ def fetch_times(limit):
 
     return times
 
-def fetch_listings(lim, query_name):
+def fetch_listings(lim, query_name, off, secondary=None):
     query = datastore_client.query(kind=query_name)
     query.order = ['-timestamp']
 
-    listings = query.fetch(limit=lim)
+    listings = query.fetch(limit=lim, offset=off)
+
     listofcars = {}
     i=1
+    if secondary == 1:
+        i=11
     for list in listings:
         listofcars.update({str(i):list})
         i=i+1
+    i=1
 
     return listofcars
 
@@ -53,36 +58,49 @@ def about():
 @app.route('/classic-bimmers')
 def classic_bimmers():
 
-    cars = fetch_listings(20, 'BMW')
+    page = request.args.get('page',1,type=int)
+    offset= (page-1)*20
+    cars = fetch_listings(20, 'BMW', offset) 
 
     return render_template('classic_bimmers.html', cars=cars)
 
 @app.route('/m-cars')
 def mcars():
 
-    cars = fetch_listings(20, 'mcar')
+    page = request.args.get('page',1,type=int)
+    offset= (page-1)*20
+    cars = fetch_listings(20, 'mcar', offset)
 
     return render_template('mcars.html', cars=cars)
 
 @app.route('/miata-hunter')
 def miata_hunter():
 
-    cars = fetch_listings(20, 'miata')
+    page = request.args.get('page',1,type=int)
+    offset= (page-1)*20
+    cars = fetch_listings(20, 'miata', offset)
 
     return render_template('miata_hunter.html', cars=cars)
 
 @app.route('/swedish-steel')
 def swedish_steel():
 
-    cars = fetch_listings(20, 'volvo')
-    cars2 = fetch_listings(20, 'Saab')
+    page = request.args.get('page',1,type=int)
+    offset= (page-1)*10
 
-    return render_template('swedish_steel.html', cars=cars)
+    cars = fetch_listings(10, 'volvo', offset)
+    cars2 = fetch_listings(10, 'Saab', offset, secondary=1)
+
+    cars_ = {**cars, **cars2}
+
+    return render_template('swedish_steel.html', cars=cars_)
     
 @app.route('/barely-driven')
 def barely_driven():
 
-    cars = fetch_listings(20, 'barely')
+    page = request.args.get('page',1,type=int)
+    offset= (page-1)*20
+    cars = fetch_listings(20, 'barely', offset)
 
     return render_template('barely_driven.html', cars=cars)
 
